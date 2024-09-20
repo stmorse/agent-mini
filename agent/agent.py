e0f866c0e73158a54cacdd7110d06e66d629f445
stmorse <steven.t.morse@gmail.com>
2024-09-20 10:41:52 -0400

    restructure repo

--- agent/agent.py
+++ agent/agent.py
@@ -4,11 +4,24 @@ import requests
 
 class Model:
     def __init__(self):
-        self.url = f'http://ollama:80/api/generate'
+        self.api_url = f'http://ollama:80/api/'
+        self.generate_endpoint = self.api_url + 'generate'
+        self.version_endpoint = self.api_url + 'version'
         self.headers = {'Content-Type': 'application/json'}
         self.name = "llama3"
 
+        self.available = False
+        self.version = None
+        try:
+            self.version = requests.post(self.version_endpoint).json()
+            self.available = True
+        except:
+            return 'Error retrieving model.'
+
     def get_response(self, prompt):
+        if not self.available:
+            return 'No model available.'
+
         data = {
             'model': self.name,
             'prompt': prompt,
@@ -16,16 +29,16 @@ class Model:
         }
 
         response = requests.post(
-            self.url,
+            self.generate_endpoint,
             headers=self.headers,
             data=json.dumps(data)
         )
 
         return response.json()
 
-print('Test!')
-
 if __name__ == "__main__":
+    print('agent.py is running...')
+
     # initialize the model wrapper
     # currently hard-coded for llama3
     model = Model()
