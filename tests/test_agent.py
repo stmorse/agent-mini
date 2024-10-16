031905d1fbc7feaaf23de561d3739e170830755e
stmorse <steven.t.morse@gmail.com>
2024-10-16 14:04:34 -0400

    small updates to test agent

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,28 +1,35 @@
 import requests
 import json
 
-# from utilities.faiss_utils import *
+from utilities.faiss_utils import *
 
 if __name__ == '__main__':
+    
+    print('Agent is running ...')
+
     # FAISS server tests
 
+    print('Testing FAISS server ...')
+
     # initialize the manager
-    # manager = get_faiss_manager(a=('faiss', 80), key=b'faiss')
-    # create_faiss_index(manager, 1, 2)
+    manager = get_faiss_manager(a=('faiss', 90), key=b'faiss')
+    create_faiss_index(manager, 1, 2)
 
-    # # add vectors to the index
-    # vectors = [[1.0, 2.0], [3.0, 4.0]]
-    # add_vectors(manager, 1, vectors, [1, 2])
+    # add vectors to the index
+    vectors = [[1.0, 2.0], [3.0, 4.0]]
+    add_vectors(manager, 1, vectors, [1, 2])
 
-    # # search the index
-    # queries = [[1.0, 2.0], [3.0, 4.0], [1.5, 2.5]]
-    # search_index(manager, 1, queries, k=1)
+    # search the index
+    queries = [[1.0, 2.0], [3.0, 4.0], [1.5, 2.5]]
+    search_index(manager, 1, queries, k=1)
 
 
     # LoRAX server tests
 
+    print('Testing LoRAX server ...')
+
     prompt = {
-        'inputs': 'translate English to French: Hello, my name is Trenton.',
+        'inputs': 'Translate English to French: Hello, my name is Trenton.',
         'parameters': {
             'max_new_tokens': 100
         }
